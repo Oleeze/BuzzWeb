175f872cbf7f68bf06ee56c79e5afe97157de253
Oleg Rudenko <orudenkony@gmail.com>
2018-10-06 12:42:15 -0700

    Created styles but not pushing because theyre too big at the moment, will deploy on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
       }
     ]
   }
